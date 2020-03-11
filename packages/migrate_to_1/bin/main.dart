4839f209f6938c750d171253d2aa9c02e1bb6807
Amir Hardon <amirha@google.com>
2020-03-11 13:44:11 -0700

    [migrate_to_1] fix retval plumbing

--- packages/migrate_to_1/bin/main.dart
+++ packages/migrate_to_1/bin/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:migrate_to_1/executable.dart' as executable;
 
 Future<int> main(List<String> arguments) async {
-  return await executable.main(arguments);
+  int retval = await executable.main(arguments);
+  if (retval != null) {
+    exit(retval);
+  }
 }
